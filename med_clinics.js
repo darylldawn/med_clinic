36935f21f4038fddd28f9622d208634dc91a393e
Daryll Dawn <daryllvillaos@gmail.com>
2013-05-16 15:59:44 +0800

    2.0
    - Working login, logout via cookies.
    - Add, edit and delete working

--- med_clinics.js
+++ med_clinics.js
@@ -1,27 +1,35 @@
 // JavaScript Document
 var qsParm = new Array();
 var apptID;
-var year, day, month, clinicID, uname, pword;
+var year, day, month, clinicID;
 var id, uname, pword;
+
 window.onload = function() {
-	//start();
-	forms();
+	trial();
+	//forms();
 }
-function start() {
-	//alert("Submitted");
-	//navigator.app.clearCache();
-	//uname=prompt("Enter your username", "");
-	//pword=prompt("Enter your password", "");
-	//if(uname == "medicard" && pword == "admin") getNews();
-	//else alert("Invalid username or password");
-	uname = prompt("Please enter your username:", "")
-	pword = prompt("Please enter your password:", "")
-	if (uname == "medicard" && pword == "m0b33l1ty")
-		getClinics();
-	else {
-		alert("Username or password is invalid");
-		document.location = "http://66.175.220.37/medicard/mcard/admin/clinics.html";
+function trial() {
+	var haha = $.cookie('haha');
+	if (haha !== '1') {
+		uname = prompt("Please enter your username:", "");
+		pword = prompt("Please enter your password:", "");
+		if (uname == "medicard" && pword == "m0b33l1ty") {
+			$.cookie('haha', '1');
+			forms();
+		} else {
+			$.cookie('haha', null);
+			alert("Invalid Login");
+			document.location = "http://66.175.220.37/medicard/mcard/admin/play.html";
+		}
+	} else {
+		forms();
 	}
+}
+
+function logout() {
+	$.cookie('haha', null);
+	alert('Logging out..');
+	document.location = "http://66.175.220.37/medicard/mcard/admin/play.html";
 }
 
 function forms() {
@@ -61,7 +69,6 @@ function forms() {
 				var myNode = document.getElementById("top");
 				myNode.innerHTML = '';
 				getClinics();
-
 			},
 
 			Cancel : function() {
@@ -69,7 +76,6 @@ function forms() {
 			}
 		},
 		close : function() {
-
 		}
 	});
 	$("#dialog-form-delete").dialog({
@@ -96,19 +102,51 @@ function forms() {
 	});
 }
 
-function editClinic(clinic_id, priority, longitude, latitude) {
-	//alert(sched.toString() + " " + dat);
+/*function editClinic(clinic_id) {
+ oFormObject = document.forms['editClinicsForm'];
+ oFormObject.elements["clinic_id"].value = clinic_id;
+ //oFormObject.elements["name"].value = daryll[1];
+ $("#dialog-form-edit").dialog("open");
+ }*/
+
+function editClinic(clinic_id) {
+	var events = '';
+	//alert("Getting news");
+	var myNode = document.getElementById("top");
 	oFormObject = document.forms['editClinicsForm'];
-	oFormObject.elements["clinic_id"].value = clinic_id;
-	oFormObject.elements["priority"].value = priority;
-	//oFormObject.elements["landline"].value = landline;
-	oFormObject.elements["longitude"].value = longitude;
-	oFormObject.elements["latitude"].value = latitude;
-	//oFormObject.elements["name"].value = name;
-	//apptID=id;
-	//alert(id);
-	$("#dialog-form-edit").dialog("open");
-	//
+
+	$.ajax({
+		url : 'http://66.175.220.37/medicard/mcard/admin/med_getValues.php?clinic_id=' + clinic_id,
+		type : 'GET',
+		async : true,
+		data : null,
+		crossDomain : true,
+		//dataType: 'json',
+		contentType : "application/x-www-form-urlencoded; application/json",
+		success : function(json) {
+			//var myNode = document.getElementById("top");
+			//myNode.innerHTML = '';
+			var obj = jQuery.parseJSON(json);
+			//alert(obj.appointment);
+			//alert(obj);
+			$.each(obj.cont, function(i, cont) {
+				oFormObject.elements["clinic_id"].value = clinic_id;
+				oFormObject.elements["priority"].value = cont.priority;
+				oFormObject.elements["longitude"].value = cont.longitude;
+				oFormObject.elements["latitude"].value = cont.latitude;
+				oFormObject.elements["name"].value = cont.name;
+				oFormObject.elements["landline"].value = cont.landline;
+				oFormObject.elements["clinicHours"].value = cont.clinicHour;
+				oFormObject.elements["address"].value = cont.city;
+				$("#dialog-form-edit").dialog("open");
+			});
+
+		},
+		error : function(err) {
+			//alert("Invalid username/password");
+
+		}
+	}, 'jsonp');
 
 }
 
@@ -146,7 +184,7 @@ function getClinics() {
 			//alert(obj);
 			$.each(obj.cont, function(i, cont) {
 				//alert(cont.news_id);
-				events += "<h3>Clinic ID: " + cont.clinic_id + " | Clinic Name: " + cont.name + "</h3><div>Name: " + cont.name + "<br>Priority: " + cont.priority + "<br>Clinic Hours: " + cont.clinicHours + "<br>Latitude: " + cont.latitude + "<br>Longitude: " + cont.longitude + "<br>Landline: " + cont.landline + "<br>Address: " + cont.city + "<br><ul id='icons' class='ui-widget ui-helper-clearfix'><li class='ui-state-default ui-corner-all' title='Edit'><span class='ui-icon ui-icon-pencil' onclick='editClinic(" + cont.clinic_id + "," + cont.priority + "," + cont.longitude + "," + cont.latitude + ")'></span></li><li class='ui-state-default ui-corner-all' title='Delete'><span class='ui-icon ui-icon-trash' onclick='deleteClinic(" + cont.clinic_id + ")'></span></li></ul></div>";
+				events += "<h3>Clinic ID: " + cont.clinic_id + " | Clinic Name: " + cont.name + "</h3><div>Name: " + cont.name + "<br>Priority: " + cont.priority + "<br>Clinic Hours: " + cont.clinicHours + "<br>Latitude: " + cont.latitude + "<br>Longitude: " + cont.longitude + "<br>Landline: " + cont.landline + "<br>Address: " + cont.city + "<br><ul id='icons' class='ui-widget ui-helper-clearfix'><li class='ui-state-default ui-corner-all' title='Edit'><span class='ui-icon ui-icon-pencil' onclick='editClinic(" + cont.clinic_id + ")'></span></li><li class='ui-state-default ui-corner-all' title='Delete'><span class='ui-icon ui-icon-trash' onclick='deleteClinic(" + cont.clinic_id + ")'></span></li></ul></div>";
 			});
 
 			document.getElementById("top").innerHTML = "<div id='accordion'>" + events + "</div>";
@@ -204,4 +242,3 @@ function changeClinic() {
 	//alert(clinicID);
 	getAllAppointments();
 }
-
